2ba4caa527b1cd146f8d63fa1c019718e0dee381
hudson221b <241zxy@gmail.com>
2024-05-28 12:12:51 -0400

    remove useless layouts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <h1>This is root layout.tsx</h1>
         <UIProviders>{children}</UIProviders>
       </body>
     </html>
--- src/app/members/layout.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-export const MembersLayout = () => {
-  return <div className="bg-green-500">MembersLayout</div>
-}
