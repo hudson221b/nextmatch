ea121bf6874819b35046cc92125d1293d46228c4
hudson221b <241zxy@gmail.com>
2024-05-28 14:27:05 -0400

    add styles and more items to navbar

--- src/components/navbar/TopNav.tsx
+++ src/components/navbar/TopNav.tsx
@@ -1,19 +1,49 @@
-import { Navbar, NavbarBrand, NavbarContent } from "@nextui-org/react"
+import {
+  Button,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+} from "@nextui-org/react"
+import Link from "next/link"
 import React from "react"
 import { GiMatchTip } from "react-icons/gi"
 
 export const TopNav = () => {
   return (
-    <Navbar>
-      <NavbarBrand>
+    <Navbar
+      maxWidth="xl"
+      className="bg-gradient-to-t from-purple-400 to-purple-700"
+      classNames={{
+        item: ["text-xl", "text-white", "upper"],
+      }}
+    >
+      <NavbarBrand as={Link} href="/">
         <GiMatchTip size={40} />
         <div>
           <span>Next</span>
           <span>Match</span>
         </div>
       </NavbarBrand>
-      <NavbarContent>Center</NavbarContent>
-      <NavbarContent>Right</NavbarContent>
+      <NavbarContent>
+        <NavbarItem as={Link} href="/members">
+          Matches
+        </NavbarItem>
+        <NavbarItem as={Link} href="/lists">
+          Lists
+        </NavbarItem>
+        <NavbarItem as={Link} href="/messages">
+          Messages
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent>
+        <Button variant="bordered" className="text-white">
+          Login
+        </Button>
+        <Button variant="bordered" className="text-white">
+          Register
+        </Button>
+      </NavbarContent>
     </Navbar>
   )
 }
