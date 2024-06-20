8edbd6a16c649542c80cbf69149acd21d6ee2abb
beizy <241zxy@gmail.com>
2024-06-20 15:47:28 -0400

    add UserMenu component

---
+++ src/components/navbar/UserMenu.tsx
@@ -0,0 +1,53 @@
+"use client"
+import {
+  Avatar,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownSection,
+  DropdownTrigger,
+} from "@nextui-org/react"
+import type { Session } from "next-auth"
+import { signOut } from "next-auth/react"
+import Link from "next/link"
+import React from "react"
+
+type UserMenuProps = {
+  user: Session["user"]
+}
+
+export default function UserMenu({ user }: UserMenuProps) {
+  return (
+    <Dropdown>
+      <DropdownTrigger>
+        <Avatar
+          isBordered
+          as="button"
+          className="transition-transform"
+          color="secondary"
+          name={user?.name || "user avatar"}
+          size="sm"
+          src=""
+        />
+      </DropdownTrigger>
+      <DropdownMenu variant="flat" aria-label="user actions menu">
+        <DropdownSection showDivider>
+          <DropdownItem isReadOnly as="span" className="h-14 flex flex-grow">
+            Now logged in as {user?.name}
+          </DropdownItem>
+        </DropdownSection>
+        <DropdownItem as={Link} href="/members/edit">
+          Edit Profile
+        </DropdownItem>
+        <DropdownItem
+          key="delete"
+          className="text-danger"
+          color="danger"
+          onClick={async () => signOut()}
+        >
+          Log out
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  )
+}
