05f05cb5fc7c4b6e88b1c65e6865e30a73a881c4
beizy <241zxy@gmail.com>
2024-06-23 15:52:05 -0400

    seed members data

---
+++ prisma/seed.ts
@@ -0,0 +1,46 @@
+import { PrismaClient } from "@prisma/client"
+import { membersData } from "./membersData"
+import { hashSync } from "bcryptjs"
+
+const prisma = new PrismaClient()
+
+async function seedMembers() {
+  return membersData.map(async member =>
+    prisma.user.create({
+      data: {
+        name: member.name,
+        email: member.email,
+        passwordHash: hashSync("123456", 10),
+        member: {
+          create: {
+            name: member.name,
+            gender: member.gender,
+            dateOfBirth: new Date(member.dateOfBirth),
+            created: new Date(member.created),
+            updated: new Date(member.lastActive),
+            description: member.description,
+            city: member.city,
+            country: member.country,
+            image: member.image,
+            photos: {
+              create: {
+                url: member.image,
+              },
+            },
+          },
+        },
+      },
+    })
+  )
+}
+
+async function main() {
+  await seedMembers()
+}
+
+main()
+  .catch(e => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => await prisma.$disconnect())
