25307058bdf6388a7ad772e68510577e3cc66b0e
beizy <241zxy@gmail.com>
2024-07-06 15:46:38 -0400

    add all input fields to edit form

--- src/app/members/edit/EditForm.tsx
+++ src/app/members/edit/EditForm.tsx
@@ -1,21 +1,88 @@
 "use client"
+import React from "react"
+import { Button, Input, Textarea } from "@nextui-org/react"
 import {
   type MemberEditSchema,
   memberEditSchema,
 } from "@/lib/schemas/member-edit-schema"
 import { zodResolver } from "@hookform/resolvers/zod"
-import React from "react"
 import { useForm } from "react-hook-form"
+import type { Member } from "@prisma/client"
 
-export default function EditForm() {
+type EditFormProp = {
+  member: Member
+}
+
+export function EditForm({ member }: EditFormProp) {
   const {
     register,
     handleSubmit,
-    reset,
-    formState: { isValid, isDirty },
+    formState: { isValid, isDirty, errors, isSubmitting },
   } = useForm<MemberEditSchema>({
     resolver: zodResolver(memberEditSchema),
+    mode: "onTouched",
   })
 
-  return <div></div>
+  const onSubmit = async (data: MemberEditSchema) => {
+    // const result = await editUser(data)
+
+    // if (result.status === "success") {
+    //   // router.push("./members")
+    // } else {
+    //   console.log(result.error)
+    //   toast.error(result.error as string,{hideProgressBar: true});
+
+    // }
+    console.log("submitting")
+  }
+
+  return (
+    <form className="flex flex-col space-y-4" onSubmit={handleSubmit(onSubmit)}>
+      <Input
+        {...register("name")}
+        label="Name"
+        variant="bordered"
+        defaultValue={member.name}
+        isInvalid={!!errors.name}
+        errorMessage={errors.name?.message}
+      />
+      <Textarea
+        {...register("description")}
+        label="Description"
+        variant="bordered"
+        defaultValue={member.description}
+        isInvalid={!!errors.description}
+        errorMessage={errors.description?.message}
+        minRows={6}
+      />
+      <div className="flex flex-row gap-3">
+        <Input
+          {...register("city")}
+          label="City"
+          variant="bordered"
+          defaultValue={member.city}
+          isInvalid={!!errors.city}
+          errorMessage={errors.city?.message}
+        />
+        <Input
+          {...register("country")}
+          label="Country"
+          variant="bordered"
+          defaultValue={member.country}
+          isInvalid={!!errors.country}
+          errorMessage={errors.country?.message}
+        />
+      </div>
+      <Button
+        color="secondary"
+        type="submit"
+        isDisabled={!isValid || !isDirty}
+        isLoading={isSubmitting}
+        className="flex self-end"
+      >
+        Update Profile
+      </Button>
+    </form>
+  )
 }
+
--- src/app/members/edit/page.tsx
+++ src/app/members/edit/page.tsx
@@ -1,8 +1,15 @@
 import { CardHeader, Divider, CardBody } from "@nextui-org/react"
 import React from "react"
 import EditForm from "./EditForm"
+import { getCurrentUserId } from "@/app/actions/authActions"
+import { getMemberById } from "@/app/actions/memberActions"
+import { notFound } from "next/navigation"
 
-export default function MemberEditPage() {
+export default async function MemberEditPage() {
+  const userId = await getCurrentUserId()
+  const member = await getMemberById(userId)
+  if (!member) return notFound()
+
   return (
     <>
       <CardHeader className="text-2xl font-semibold text-secondary">
@@ -10,7 +17,7 @@ export default function MemberEditPage() {
       </CardHeader>
       <Divider />
       <CardBody>
-        <EditForm />
+        <EditForm member={member}/>
       </CardBody>
     </>
   )
