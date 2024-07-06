dda187ea6d61d9482aa5c0850e9322a38f015eca
beizy <241zxy@gmail.com>
2024-07-06 13:30:26 -0400

    add EditForm dummy file

--- src/app/members/edit/page.tsx
+++ src/app/members/edit/page.tsx
@@ -1,5 +1,17 @@
+import { CardHeader, Divider, CardBody } from "@nextui-org/react"
 import React from "react"
+import EditForm from "./EditForm"
 
 export default function MemberEditPage() {
-  return <div>MemberEditPage</div>
+  return (
+    <>
+      <CardHeader className="text-2xl font-semibold text-secondary">
+        Edit Profile
+      </CardHeader>
+      <Divider />
+      <CardBody>
+        <EditForm />
+      </CardBody>
+    </>
+  )
 }
