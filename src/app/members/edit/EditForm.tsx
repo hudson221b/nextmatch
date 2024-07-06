85d0b516802a989bcc14c76cc4b58bf3e4d8fee3
beizy <241zxy@gmail.com>
2024-07-06 14:48:38 -0400

    add new edit form schema and form hook

--- src/app/members/edit/EditForm.tsx
+++ src/app/members/edit/EditForm.tsx
@@ -1,5 +1,21 @@
+"use client"
+import {
+  type MemberEditSchema,
+  memberEditSchema,
+} from "@/lib/schemas/member-edit-schema"
+import { zodResolver } from "@hookform/resolvers/zod"
 import React from "react"
+import { useForm } from "react-hook-form"
 
 export default function EditForm() {
-  return <div>EditForm</div>
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { isValid, isDirty },
+  } = useForm<MemberEditSchema>({
+    resolver: zodResolver(memberEditSchema),
+  })
+
+  return <div></div>
 }
