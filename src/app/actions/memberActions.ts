e3ff84b48de7dd336a4ae1e2eae669cc86dcb2d2
beizy <241zxy@gmail.com>
2024-07-07 16:11:23 -0400

    add updateMemberProfile server action

--- src/app/actions/memberActions.ts
+++ src/app/actions/memberActions.ts
@@ -2,7 +2,13 @@
 
 import { auth } from "@/auth"
 import { prisma } from "@/lib/prisma"
-import type { Photo } from "@prisma/client"
+import {
+  type MemberEditSchema,
+  memberEditSchema,
+} from "@/lib/schemas/member-edit-schema"
+import type { Member, Photo } from "@prisma/client"
+import { getCurrentUserId } from "./authActions"
+import type { ActionResult } from "@/types"
 
 /**
  * get all members except oneself after login
@@ -44,5 +50,36 @@ export const getMemberPhotosByUserId = async (userId: string) => {
     return member.photos as Photo[]
   } catch (error) {
     console.error("Error finding a member")
+  }
+}
+
+export const updateMemberProfile = async (
+  data: MemberEditSchema
+): Promise<ActionResult<Member>> => {
+  try {
+    const userId = await getCurrentUserId()
+    const validated = memberEditSchema.safeParse(data)
+    if (!validated.success) {
+      return { status: "error", error: validated.error.issues }
+    }
+
+    const { name, description, city, country } = validated.data
+
+    const member = await prisma.member.update({
+      where: { userId },
+      data: {
+        name,
+        description,
+        city,
+        country,
+      },
+    })
+    return { status: "success", data: member }
+  } catch (error) {
+    console.error(error)
+    return {
+      status: "error",
+      error: "Error updating member profile",
+    }
   }
 }
--- src/app/members/edit/page.tsx
+++ src/app/members/edit/page.tsx
@@ -1,6 +1,6 @@
 import { CardHeader, Divider, CardBody } from "@nextui-org/react"
 import React from "react"
-import EditForm from "./EditForm"
+import { EditForm } from "./EditForm"
 import { getCurrentUserId } from "@/app/actions/authActions"
 import { getMemberById } from "@/app/actions/memberActions"
 import { notFound } from "next/navigation"
