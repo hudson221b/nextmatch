fb35af8e9d994ed16e2d7af9317398f8cb3a0d8d
beizy <241zxy@gmail.com>
2024-07-22 11:49:43 -0400

    config global pusher instances

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -1,10 +1,30 @@
-import Pusher from "pusher"
+// client library
+import PusherClient from "pusher-js"
+
+// server library
+import PusherServer from "pusher"
 
 declare global {
-  pusherClient: PusherClient | undefined
-  pusherServer: PusherServer | undefined
+  var pusherClient: PusherClient | undefined
+  var pusherServer: PusherServer | undefined
 }
 
-if (!pusherClient) {
-  global.pusherClient = new Pusher()
+if (!global.pusherClient) {
+  global.pusherClient = new PusherClient(process.env.PUSHER_APP_KEY!, {
+    cluster: process.env.PUSHER_CLUSTER!,
+    forceTLS: true,
+  })
 }
+
+if (!global.pusherServer) {
+  global.pusherServer = new PusherServer({
+    appId: process.env.PUSHER_APP_ID!,
+    key: process.env.PUSHER_APP_KEY!,
+    secret: process.env.PUSHER_SECRET!,
+    cluster: process.env.PUSHER_CLUSTER!,
+    useTLS: true,
+  })
+}
+
+export const pusherClient = global.pusherClient
+export const pusherServer = global.pusherServer
