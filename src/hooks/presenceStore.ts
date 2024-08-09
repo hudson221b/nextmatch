058f92a8e68a534febc45c24b8207f6896098a27
beizy <241zxy@gmail.com>
2024-08-09 18:12:06 -0400

    install zustand and create presence store

---
+++ src/hooks/presenceStore.ts
@@ -0,0 +1,29 @@
+import { create } from "zustand"
+import { devtools } from "zustand/middleware"
+
+type PresenceState = {
+  members: string[]
+  addMember: (id: string) => void
+  removeMember: (id: string) => void
+  setMembers: (ids: string[]) => void
+}
+
+/**
+ * Creates a global presence store to keep track of users connected to the app.
+ */
+const usePresenceStore = create<PresenceState>()(
+  devtools(
+    set => ({
+      members: [],
+      addMember: id => set(state => ({ members: [...state.members, id] })),
+      removeMember: id =>
+        set(state => ({
+          members: state.members.filter(member => member !== id),
+        })),
+      setMembers: ids => set({ members: ids }),
+    }),
+    {
+      name: "presence_store",
+    }
+  )
+)
