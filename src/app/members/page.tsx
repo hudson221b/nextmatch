09b80748ccc16077a66ccf73f7ba9e8e4b25168c
beizy <241zxy@gmail.com>
2024-08-15 14:46:30 -0400

    fix where condition in getMembers to exclude oneself

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -7,8 +7,9 @@ import MemberCard from "./memberCard"
 
 const MembersPage: React.FC = async () => {
   const members = await getMembers()
+
   // members that the current user has liked
-  const likeIds = await fetchLikesForCurrentUser("source", "id") as string[]
+  const likeIds = (await fetchLikesForCurrentUser("source", "id")) as string[]
   return (
     <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-8">
       {members &&
