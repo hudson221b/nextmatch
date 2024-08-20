8a48bb12ccabda0e3bcae0575092159782679bf9
beizy <241zxy@gmail.com>
2024-08-20 10:28:48 -0400

    set unread count on login

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,11 +1,13 @@
 "use client"
 
+import "react-toastify/ReactToastify.css"
 import { NextUIProvider } from "@nextui-org/react"
-import React, { type ReactNode } from "react"
+import React, { type ReactNode, useRef, useEffect } from "react"
 import { usePresenceChannel } from "@/hooks/usePresenceChannel"
 import { ToastContainer } from "react-toastify"
-import "react-toastify/ReactToastify.css"
 import { useNotificationChannel } from "@/hooks/useNotificationChannel"
+import { useMessagesStore } from "@/hooks/useStores"
+import { getUnreadMsgCount } from "@/app/actions/messageActions"
 
 export const UIProviders = ({
   children,
@@ -17,6 +19,20 @@ export const UIProviders = ({
   usePresenceChannel(userId)
   useNotificationChannel(userId)
 
+  // set unread messages count on login
+  const setUnreadCount = useMessagesStore(state => state.setUnreadCount)
+
+  const isUnreadCountSet = useRef<boolean>(false)
+
+  useEffect(() => {
+    if (!userId) return
+
+    if (isUnreadCountSet.current) return
+
+    getUnreadMsgCount().then(count => setUnreadCount(count))
+    isUnreadCountSet.current = true
+  }, [userId])
+
   return (
     <NextUIProvider>
       <ToastContainer position="bottom-right" />
@@ -24,3 +40,5 @@ export const UIProviders = ({
     </NextUIProvider>
   )
 }
+
+
