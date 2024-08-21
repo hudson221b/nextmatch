e5df1d1633e185f0996413fe392937af7411ea23
beizy <241zxy@gmail.com>
2024-08-21 16:30:52 -0400

    add paginatioComponent UI

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -4,6 +4,7 @@ import {
   fetchLikesForCurrentUser,
 } from "../actions/likeActions"
 import MemberCard from "./memberCard"
+import PaginationComponent from "@/components/PaginationComponent"
 
 const MembersPage: React.FC = async () => {
   const members = await getMembers()
@@ -11,18 +12,21 @@ const MembersPage: React.FC = async () => {
   // members that the current user has liked
   const likeIds = (await fetchLikesForCurrentUser("source", "id")) as string[]
   return (
-    <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-8">
-      {members &&
-        members.map(member => {
-          return (
-            <MemberCard
-              member={member}
-              key={`${member.id}-member-card`}
-              likeIds={likeIds}
-            />
-          )
-        })}
-    </div>
+    <>
+      <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-8">
+        {members &&
+          members.map(member => {
+            return (
+              <MemberCard
+                member={member}
+                key={`${member.id}-member-card`}
+                likeIds={likeIds}
+              />
+            )
+          })}
+      </div>
+      <PaginationComponent />
+    </>
   )
 }
 export default MembersPage
