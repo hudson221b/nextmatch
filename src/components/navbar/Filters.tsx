004e4024fe90b086d5c247794df50dbdbc572542
beizy <241zxy@gmail.com>
2024-08-21 14:50:44 -0400

    add filter component under navbar

---
+++ src/components/navbar/Filters.tsx
@@ -0,0 +1,94 @@
+"use client"
+
+import { Button } from "@nextui-org/button"
+import { SelectItem } from "@nextui-org/react"
+import { Select } from "@nextui-org/select"
+import { Slider } from "@nextui-org/slider"
+import { usePathname } from "next/navigation"
+import React, { useMemo } from "react"
+import { FaFemale, FaMale } from "react-icons/fa"
+
+export default function Filters() {
+  const path = usePathname()
+
+  const genderItems = useMemo(
+    () => [
+      {
+        value: "male",
+        icon: FaMale,
+      },
+      {
+        value: "female",
+        icon: FaFemale,
+      },
+    ],
+    []
+  )
+
+  const orderByItems = useMemo(
+    () => [
+      {
+        label: "Last active",
+        value: "updated",
+      },
+      {
+        label: "Newest members",
+        value: "created",
+      },
+    ],
+    []
+  )
+
+  if (path !== "/members") return null
+
+  return (
+    <div
+      id="filters-wrapper"
+      className="flex justify-around items-center shadow-md py-2"
+    >
+      <div
+        id="result-container"
+        className="text-secondary font-semibold text-xl"
+      >
+        Results: 10
+      </div>
+      <div
+        id="gender-selection-buttons-container"
+        className="flex gap-2 items-center"
+      >
+        <div>Gender:</div>
+        {genderItems.map(({ value, icon: Icon }) => (
+          <Button
+            isIconOnly
+            aria-label={value}
+            key={`${value}-button`}
+            color="secondary"
+            size="sm"
+          >
+            <Icon size={24} />
+          </Button>
+        ))}
+      </div>
+      <div id="age-slider-container" className="flex flex-row w-1/4">
+        <Slider
+          label="Age range"
+          step={1}
+          maxValue={100}
+          minValue={18}
+          defaultValue={[25, 55]}
+          size="sm"
+          color="secondary"
+        />
+      </div>
+      <div id="order-by-selector-container" className="w-1/4">
+        <Select label="Order by" size="sm" variant="bordered">
+          {orderByItems.map(item => (
+            <SelectItem key={`${item.value}-option`} value={item.value}>
+              {item.label}
+            </SelectItem>
+          ))}
+        </Select>
+      </div>
+    </div>
+  )
+}
--- src/components/navbar/TopNav.tsx
+++ src/components/navbar/TopNav.tsx
@@ -7,61 +7,65 @@ import { auth } from "@/auth"
 import UserMenu from "./UserMenu"
 import { getUserById } from "@/app/actions/authActions"
 import type { User } from "@prisma/client"
+import Filters from "./Filters"
 
 export const TopNav = async () => {
   const session = await auth()
   const latestUser:User | undefined | null = session?.user &&  await getUserById(session.user.id as string)
     
   return (
-    <Navbar
-      maxWidth="xl"
-      className="bg-gradient-to-r from-purple-400 to-purple-700"
-      classNames={{
-        item: [
-          "text-xl",
-          "text-white",
-          "uppercase",
-          "data-[active=true]:text-yellow-200",
-        ],
-      }}
-    >
-      <NavbarBrand as={Link} href="/">
-        <GiMatchTip size={40} className="text-gray-200" />
-        <div className="font-bold text-3xl">
-          <span className="text-gray-900">Next</span>
-          <span className="text-gray-200">Match</span>
-        </div>
-      </NavbarBrand>
-      <NavbarContent>
-        <NavLink href="/members" label="Members" />
-        <NavLink href="/lists" label="Lists" />
-        <NavLink href="/messages" label="Messages" />
-      </NavbarContent>
-      <NavbarContent justify="end">
-        {latestUser ? (
-          <UserMenu user={latestUser} />
-        ) : (
-          <>
-            <Button
-              variant="bordered"
-              className="text-white"
-              as={Link}
-              href="/login"
-            >
-              Login
-            </Button>
-            <Button
-              variant="bordered"
-              className="text-white"
-              as={Link}
-              href="/register"
-            >
-              Register
-            </Button>
-          </>
-        )}
-      </NavbarContent>
-    </Navbar>
+    <>
+      <Navbar
+        maxWidth="xl"
+        className="bg-gradient-to-r from-purple-400 to-purple-700"
+        classNames={{
+          item: [
+            "text-xl",
+            "text-white",
+            "uppercase",
+            "data-[active=true]:text-yellow-200",
+          ],
+        }}
+      >
+        <NavbarBrand as={Link} href="/">
+          <GiMatchTip size={40} className="text-gray-200" />
+          <div className="font-bold text-3xl">
+            <span className="text-gray-900">Next</span>
+            <span className="text-gray-200">Match</span>
+          </div>
+        </NavbarBrand>
+        <NavbarContent>
+          <NavLink href="/members" label="Members" />
+          <NavLink href="/lists" label="Lists" />
+          <NavLink href="/messages" label="Messages" />
+        </NavbarContent>
+        <NavbarContent justify="end">
+          {latestUser ? (
+            <UserMenu user={latestUser} />
+          ) : (
+            <>
+              <Button
+                variant="bordered"
+                className="text-white"
+                as={Link}
+                href="/login"
+              >
+                Login
+              </Button>
+              <Button
+                variant="bordered"
+                className="text-white"
+                as={Link}
+                href="/register"
+              >
+                Register
+              </Button>
+            </>
+          )}
+        </NavbarContent>
+      </Navbar>
+      <Filters />
+    </>
   )
 }
 
