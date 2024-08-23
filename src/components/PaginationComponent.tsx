779887f214bd5ce088266e30101a5a30e6808595
beizy <241zxy@gmail.com>
2024-08-23 11:00:05 -0400

    use store in pagination component

--- src/components/PaginationComponent.tsx
+++ src/components/PaginationComponent.tsx
@@ -1,23 +1,49 @@
 "use client"
+import { usePaginationStore } from "@/hooks/useStores"
 import { Pagination } from "@nextui-org/react"
-import React, { useState } from "react"
+import { useEffect, useMemo } from "react"
 
 export default function PaginationComponent() {
-  const [pageSize, setPageSize] = useState(4)
+  const { pagination, setTotalCount, setPageNumber, setPageSize } =
+    usePaginationStore()
+
+  const totalCount = 20
+
+  useEffect(() => {
+    setTotalCount(totalCount)
+  }, [setTotalCount])
+
+  const { pageNumber, pageSize, totalPages } = pagination
+
+  const resultStart = useMemo(
+    () => (pageNumber - 1) * pageSize + 1,
+    [pageNumber, pageSize]
+  )
+
+  const resultEnd = useMemo(
+    () => Math.min(pageSize * pageNumber, totalCount),
+    [pageNumber, pageSize, totalCount]
+  )
+
+  const resultText = useMemo(
+    () => `Showing ${resultStart}-${resultEnd} of ${totalCount} results`,
+    [resultEnd, resultStart, totalCount]
+  )
+
   return (
     <div
       id="pagination-wrapper"
       className="flex justify-between py-2 border-t-2 mt-5 w-full"
     >
-      <div id="pagination-showing-result-container">
-        Showing 1-10 of 23 results
-      </div>
+      <div id="pagination-showing-result-container">{resultText}</div>
       <div id="pagination-page-selector-container">
         <Pagination
-          total={20}
+          total={totalPages}
           initialPage={1}
+          page={pageNumber}
           variant="bordered"
           color="secondary"
+          onChange={setPageNumber}
         />
       </div>
       <div
@@ -33,6 +59,7 @@ export default function PaginationComponent() {
                 : "hover:bg-gray-100"
             }`}
             key={`page-size-${size}`}
+            onClick={() => setPageSize(size)}
           >
             {size}
           </div>
